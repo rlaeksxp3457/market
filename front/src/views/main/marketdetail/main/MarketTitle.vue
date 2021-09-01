e8f09f357126ebd1a1e8627fa91cf4c99e1c1aab
s2ryu1 <xenn23@kakao.com>
2021-09-01 15:56:46 +0900

    size

--- front/src/views/main/marketdetail/main/MarketTitle.vue
+++ front/src/views/main/marketdetail/main/MarketTitle.vue
@@ -4,44 +4,61 @@
       justify="space-between"
     >
       <v-col
-        class="ml-15"
-        cols="4"
+        cols="6"
+        align="center"
       >
         <v-img
-          class="my-10"
+          class="my-5"
           lazy-src="https://picsum.photos"
-          max-height="600"
-          max-width="600"
           src="https://picsum.photos/700"
+          :max-height="`${Img_size}`"
+          :max-width="`${Img_size}`"
         />
       </v-col>
       <v-col
-        align-self="start"
-        xl="7"
+        cols="6"
       >
-        <div class="text-h3 text-left font-weight-bold my-16">
-          백종원이 인정한 연돈 <small>일식</small>
-        </div>
-        <v-row>
-          <v-col
-            xl="1"
+        <v-col
+          cols="12"
+        >
+          <div
+            class="text-h3 text-left font-weight-bold mt-5 mb-3"
+            :class="`text-${Font_size}`"
           >
-            <v-rating
-              background-color="grey"
-              class="star"
-              color="warning"
-              dense
-              half-increments
-              length="5"
-              readonly
-              size="45"
-              :value="ratingStar"
-            />
-            <!-- <span class="rat-score">4.5</span> -->
-          </v-col>
-        </v-row>
-        <h4>경기도 김포시 00동 000-00</h4>
-        <h4>031-000-0000</h4>
+            백종원이 인정한 연돈 <small>일식</small>
+          </div>
+        </v-col>
+        <v-col
+          cols="12"
+        >
+          <v-rating
+            background-color="grey"
+            class="mb-1"
+            color="warning"
+            dense
+            half-increments
+            length="5"
+            readonly
+            :size="`${Rating_size}`"
+            :value="ratingStar"
+          />
+        </v-col>
+        <v-col
+          cols="12"
+        >
+          <div
+            class="mb-3"
+            :class="`text-${Font_size2}`"
+          >
+            경기도 김포시 00동 000-00
+          </div>
+
+          <div
+            :class="`text-${Font_size2}`"
+          >
+            031-000-0000
+          </div>
+        </v-col>
       </v-col>
     </v-row>
   </v-card>
@@ -55,11 +72,43 @@ export default {
       get() {
         return this.$store.getters["marketDetail/getRating"]
       }
-    }
+    },
+    Img_size(){
+      switch (this.$vuetify.breakpoint.name){
+        case 'sm' : return '250'
+        case 'md' : return '350'
+        case 'lg' : return '400'
+        case 'xl' : return '450'
+        default : return '200'
+      }
+    },
+    Rating_size(){
+      switch (this.$vuetify.breakpoint.name){
+        case 'sm' : return '40'
+        case 'md' : return '40'
+        case 'lg' : return '50'
+        case 'xl' : return '60'
+        default : return '20'
+      }
+    },
+    Font_size(){
+      switch (this.$vuetify.breakpoint.name){
+        case 'sm' : return 'sm-h5'
+        case 'md' : return 'md-h4'
+        case 'lg' : return 'lg-h4'
+        case 'xl' : return 'xl-h3'
+        default : return 'subtitle-1'
+      }
+    },
+    Font_size2(){
+      switch (this.$vuetify.breakpoint.name){
+        case 'sm' : return 'sm-body-2'
+        case 'md' : return 'md-h6'
+        case 'lg' : return 'lg-h6'
+        case 'xl' : return 'xl-h5'
+        default : return 'caption'
+      }
+    },
   }
 }
 </script>
-
-<style scoped>
-
-</style>
