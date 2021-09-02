f320ccd92ff6db9149fe5f35c96ce02d21b616f6
s2ryu1 <xenn23@kakao.com>
2021-09-02 17:52:57 +0900

    option button, items

---
+++ front/src/views/market/menu/option/OptionButton.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-speed-dial
+    v-model="fab"
+    :top="top"
+    :bottom="bottom"
+    :right="right"
+    :left="left"
+    :direction="direction"
+    :open-on-hover="hover"
+    :transition="transition"
+  >
+    <template #activator>
+      <v-btn
+        v-model="fab"
+        color="blue darken-2"
+        dark
+        fab
+      >
+        <v-icon v-if="fab">
+          mdi-close
+        </v-icon>
+        <v-icon v-else>
+          mdi-pencil
+        </v-icon>
+      </v-btn>
+    </template>
+    <v-btn
+      fab
+      dark
+      small
+      color="green"
+    >
+      <v-icon>mdi-done-all</v-icon>
+    </v-btn>
+    <v-btn
+      fab
+      dark
+      small
+      color="indigo"
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+  </v-speed-dial>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    direction: 'top',
+    fab: false,
+    fling: false,
+    hover: false,
+    tabs: null,
+    top: false,
+    right: true,
+    bottom: true,
+    left: false,
+    transition: 'slide-y-reverse-transition',
+  }),
+
+  watch: {
+    top (val) {
+      this.bottom = !val
+    },
+    right (val) {
+      this.left = !val
+    },
+    bottom (val) {
+      this.top = !val
+    },
+    left (val) {
+      this.right = !val
+    },
+  },
+}
+</script>
--- front/src/views/market/menu/option/OptionTabItems.vue
+++ front/src/views/market/menu/option/OptionTabItems.vue
@@ -1,1 +1,21 @@
-<template />
+<template>
+  <v-container>
+    <option-select />
+
+    <v-divider />
+
+    <option-panel />
+
+    <option-button />
+  </v-container>
+</template>
+<script>
+export default {
+  name: "OptionTabItems",
+  components: {
+    OptionButton:() => import ("@/views/market/menu/option/OptionButton"),
+    OptionPanel: () => import("@/views/market/menu/option/OptionPanel"),
+    OptionSelect: () => import ("@/views/market/menu/option/OptionSelect"),
+  },
+}
+</script>
