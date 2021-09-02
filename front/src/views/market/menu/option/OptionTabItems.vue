a1a850a0ba1cf2ae7a4005bc898ba6e230be76f5
s2ryu1 <xenn23@kakao.com>
2021-09-02 17:55:48 +0900

    option button, items

--- front/src/views/market/menu/option/OptionTabItems.vue
+++ front/src/views/market/menu/option/OptionTabItems.vue
@@ -11,12 +11,12 @@
   </v-container>
 </template>
 <script>
-export default {
-  name: "OptionTabItems",
-  components: {
-    OptionButton:() => import ("@/views/market/menu/option/OptionButton"),
-    OptionPanel: () => import("@/views/market/menu/option/OptionPanel"),
-    OptionSelect: () => import ("@/views/market/menu/option/OptionSelect"),
-  },
-}
+  export default {
+    name: "OptionTabItems",
+    components: {
+      OptionButton:() => import ("@/views/market/menu/option/OptionButton"),
+      OptionPanel: () => import("@/views/market/menu/option/OptionPanel"),
+      OptionSelect: () => import ("@/views/market/menu/option/OptionSelect"),
+    },
+  }
 </script>
